aa6a8138719c0b6c4a098bdea44b7abe94d0eec5
aishan <aishan520315@vip.126.com>
2017-06-29 17:32:20 +0800

    修改默认配置文件

--- src/config/log-notice.php
+++ src/config/log-notice.php
@@ -7,7 +7,7 @@
  */
 return [
     //日志RMQ连接配置
-    'log_rmq'=>env('LOG_RMQ_ENABLE',true),//是否开启日志推送到队列
+    'log_rmq'=>env('LOG_RMQ_ENABLE',false),//是否开启日志推送到队列
     'log_rmq_connect'=>[
         'host'=>env('RMQ_LOG_HOST'),
         'port'=>env('RMQ_LOG_PORT'),
@@ -16,9 +16,9 @@ return [
         'vhost'=>env('RMQ_LOG_VHOST')
     ],
     'log_rmq_source'=>env('RMQ_SOURCE','pvm-center'),
-    'log_rmq_queue'=>env('RMQ_QUEUE','default'),
-    //日志RMQ配置
-    'log'=>[
+    'log_rmq_job_queue'=>env('RMQ_QUEUE','default'),//发送消息到rmq的动作使用的job所需指定的laravel队列
+    //日志RMQ的队列配置
+    'log_rmq_queue'=>[
         'exchange' => 'e.log.frontend',
         'routingKey' => 'r.log.frontend',
         'exchangeType' => 'direct',
@@ -28,9 +28,9 @@ return [
 
     //邮件配置
     'mail_enable'=>env('LOG_MAIL_ENABLE',false),//是否异常报错邮件发送
-    'mail_subject'=>env('LOG_MAIL_SUBJECT','Error Notice'),
-    'mail_receiver'=>env('LOG_MAIL_RECEIVER',''),
-    'mail-queue'=>env('LOG_MAIL_QUEUE','default'),
+    'mail_subject'=>env('LOG_MAIL_SUBJECT','Error Notice'),//通知邮件标题
+    'mail_receiver'=>env('LOG_MAIL_RECEIVER',''),//邮件接收者，多个收件人用逗号分隔
+    'mail_job_queue'=>env('LOG_MAIL_QUEUE','default'),//发送邮件job所所需指定的laravel队列
 
 
     //本地日志文件
